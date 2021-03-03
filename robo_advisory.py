fc3184f65460fb8e7c00946e2f7624f389a80d4f
scm127 <67708429+scm127@users.noreply.github.com>
2021-03-03 18:53:57 -0500

    Update robo_advisory.py

--- robo_advisory.py
+++ robo_advisory.py
@@ -5,9 +5,10 @@ import json
 request_url="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=demo"
 
 response = requests.get(request_url)
-
+last_refreshed= parsed_response["Meta Data"]["3. Last Refreshed"]
 
 parsed_response = json.loads(response.text)
+latest_close= parsed_response["Time Series {Daily}"]["2021-03-02"]["4. close"]
 breakpoint()
 #print(type(response))
 #print(response.status_code)
@@ -19,8 +20,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print("LATEST DAY: {last_refreshed}")
+print("LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
