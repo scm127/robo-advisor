6203efd02f65382e30d0d959a98b75cc8ca692cf
scm127 <67708429+scm127@users.noreply.github.com>
2021-03-04 20:39:17 -0500

    Update robo_advisory.py

--- robo_advisory.py
+++ robo_advisory.py
@@ -2,14 +2,30 @@
 import requests
 import json
 
+#Taken from a previous project
+def to_usd(my_price):
+    return f"${my_price:,.2f}" #> $12,000.71
+
 request_url="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=demo"
 
 response = requests.get(request_url)
 parsed_response = json.loads(response.text)
 
+tsd=parsed_response["Time Series (Daily)"]
+
+dates= list(tsd.keys())
+
+latest_day=dates[0]
+
 last_refreshed= parsed_response["Meta Data"]["3. Last Refreshed"]
 
-latest_close= parsed_response["Time Series (Daily)"]["2021-03-02"]["4. close"]
+latest_close= tsd[latest_day]["4. close"] 
+high_prices=[]
+recent_high=max(high_prices)
+
+for date in dates:
+    high_price=tsd[date]["4. close"]
+    high_prices.append(high_price)
 #breakpoint()
 #print(type(response))
 #print(response.status_code)
@@ -21,9 +37,9 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print("LATEST DAY:"+ last_refreshed)
+print("LATEST CLOSE:"+ to_usd(float(latest_close)))
+print("RECENT HIGH: "+to_usd(float(recent_high)))
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
