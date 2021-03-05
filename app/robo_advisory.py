cca119019a53bb7a1ba214a70aece25681855c38
scm127 <67708429+scm127@users.noreply.github.com>
2021-03-05 11:06:48 -0500

    update

--- app/robo_advisory.py
+++ app/robo_advisory.py
@@ -1,6 +1,8 @@
 # this is the "app/robo_advisor.py" file
 import requests
 import json
+import csv
+import os
 
 #Taken from a previous project
 def to_usd(my_price):
@@ -21,17 +23,33 @@ last_refreshed= parsed_response["Meta Data"]["3. Last Refreshed"]
 
 latest_close= tsd[latest_day]["4. close"] 
 high_prices=[]
-
+low_prices=[]
 for date in dates:
     high_price=tsd[date]["2. high"]
     high_prices.append(high_price)
+    low_price=tsd[date]["3. low"]
+    low_prices.append(float(low_price))
 
 recent_high=max(high_prices)
-
+recent_low=min(low_prices)
 #breakpoint()
 #print(type(response))
 #print(response.status_code)
 #print(response.text)
+
+
+
+import csv
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv") # a relative filepath
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -42,7 +60,7 @@ print("-------------------------")
 print("LATEST DAY:"+ last_refreshed)
 print("LATEST CLOSE:"+ to_usd(float(latest_close)))
 print("RECENT HIGH: "+to_usd(float(recent_high)))
-print("RECENT LOW: $99,000.00")
+print("RECENT LOW: "+to_usd(float(recent_low)))
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
