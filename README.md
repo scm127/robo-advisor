3998db6f13973187dcdef149a86221339e307c10
scm127 <67708429+scm127@users.noreply.github.com>
2021-03-08 21:07:41 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# robo-advisor
+# robo_advisor
+This Readme.md is adopted from my Shopping-cart Readme.  It explains how to clone the robo-advisor repository, and allows the user to pull stock data from the internet for any stock and returns the last closing price, the recent high and the recent low price and a reccomendation about whether to buy or sell the stock. 
+
+# Prerequisites:
+```sh
+Anaconda 3.7+
+Python 3.7+
+Pip
+```
+
+# Installing the Repository
+Fork this remote repository under your own control, then "clone" or download your remote copy onto your local computer.
+
+Navigate to the repository from the command line (subsequent commands assume you are running them from the local repository's root directory). If you saved the repository to your desktop use the following code or else you will have to adjust the code to wherever the repository is saved:
+```sh
+cd ~/Desktop/robo-advisor
+```
+# Create and activate virtual enviorment
+Use Anaconda to create and activate a new virtual environment, perhaps called "my-game-env":
+
+```sh
+conda create -n stocks-env python=3.8
+conda activate stocks-env
+```
+
+From inside the virtual environment, install package dependencies. The requirmemnts.txt file has the Dotenv package which is needed to load enviorment variables as well as the requests package which is nrequest data from the internet:
+```sh
+pip install -r requirements.txt
+```
+# Setup local variables
+In the root directory of your local repository, create a new file called ".env". The program needs an API key to issue requests to AlphaVantage API. Follow this link and the instructions to get a free API key:  https://www.alphavantage.co/ and create a variable called ALPHAVANTAGE_API_KEY in the .env file with your API key:
+
+```sh
+ALPHAVANTAGE_API_KEY="YOUR_API_KEY"
+```
+
+# Run the robo-advisor Python Script and follow the instructions to input valid stock symbols (eg. "MSFT") for the desired stocks:
+```sh
+python shopping_cart.py
+```
+Note that if a numeric value is input or if the input is not between 1 and 5 letters, the program will stop and ask to please enter a valid stock symbol. If there is no stock symbols that match, the program will ask to enter a valid symbol. 
+
--- app/robo_advisory.py
+++ app/robo_advisory.py
@@ -1,22 +1,42 @@
 # this is the "app/robo_advisor.py" file
+# This code was written with help from Professor Rossetti's "Robo Advisor" project walkthrough
 import requests
 import json
 import csv
 import os
 from dotenv import load_dotenv
+import time
+
 load_dotenv()
 #Taken from a previous project
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
-symbol="MSFT"
+timestr = time.strftime("%I:%M %p")
+datestr = time.strftime("%m/%d/%Y")
+symbol=input("Please input a valid stock or crypotcurency symbol: ")
+# Found the .isalpha() function from https://stackoverflow.com/questions/18667410/how-can-i-check-if-a-string-only-contains-letters-in-python
+if symbol.isalpha()==False:
+    print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
+    quit()
+if len(symbol)>5:
+    print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
+    quit()
+if len(symbol)<1:
+    print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
+    quit()
+
+
 api_key=os.environ.get("ALPHAVANTAGE_API_KEY")
 request_url=f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
 response = requests.get(request_url)
 parsed_response = json.loads(response.text)
-
-tsd=parsed_response["Time Series (Daily)"]
+try:
+    tsd=parsed_response["Time Series (Daily)"]
+except KeyError:
+    print("Please enter a valid stock symbol and try again")
+    quit()
 
 dates= list(tsd.keys())
 
@@ -32,17 +52,29 @@ for date in dates:
     high_prices.append(high_price)
     low_price=tsd[date]["3. low"]
     low_prices.append(float(low_price))
-
-recent_high=max(high_prices)
+recent_close2=tsd[dates[1]]["4. close"]
+recent100highprices=high_prices[:100]
+recent_high=max(recent100highprices)
 recent_low=min(low_prices)
 #breakpoint()
 #print(type(response))
 #print(response.status_code)
 #print(response.text)
 
+reccomendation=""
+reason=""
+if float(latest_close)>=recent_low*1.2:
+    if float(latest_close)>=float(recent_close2)*1.05:
+        reccomendation="Buy"
+        reason="You should buy because the latest close is more than 20% higher than the recent low and the most recent close is more than 5% higher than the previous day's close"
+    else:
+        reccomendation="Hold"
+        reason="You should hold the stock or not buy because the latest close is less than 5% higher than the previous day's close"
+else:
+    reccomendation="Sell"
+    reason="You should not buy the stock or sell because the latest close is within 20% of the recent low"
 
 
-import csv
 
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv") # a relative filepath
 
@@ -62,19 +94,21 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
             "volume":daily_prices["5. volume"],
         })
 
+
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: "+symbol)
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: "+timestr+" on "+datestr)
 print("-------------------------")
 print("LATEST DAY:"+ last_refreshed)
 print("LATEST CLOSE:"+ to_usd(float(latest_close)))
 print("RECENT HIGH: "+to_usd(float(recent_high)))
 print("RECENT LOW: "+to_usd(float(recent_low)))
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+print("RECOMMENDATION: "+reccomendation)
+print("RECOMMENDATION REASON: "+reason)
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
